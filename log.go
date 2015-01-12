8956e2e2b6ba37e5def3ba6a6756006e7b936697
lost <dragon829@gmail.com>
2015-01-12 15:51:08 +0800

    增加service字段

--- log.go
+++ log.go
@@ -17,44 +17,45 @@ var tags = map[string]*Logger{}
 var day_tags = map[string]*DailyLogger{}
 
 func handle_client(conn *net.TCPConn) {
-    reader := bufio.NewReader(conn)
-    
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil {
-            break
-        }
+	reader := bufio.NewReader(conn)
 
-        index := strings.IndexByte(line, ':')
-        if index == -1 {
-            continue
-        }
-        tag := line[0:index]
-        logger, present := tags[tag]
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
 
-        day_logger, day_present := day_tags[tag]
-        if !present && !day_present {
-            continue
-        }
+		index := strings.IndexByte(line, ':')
+		if index == -1 {
+			continue
+		}
+		tag := line[0:index]
+		logger, present := tags[tag]
 
-        l := line[index+1:]
+		day_logger, day_present := day_tags[tag]
+		if !present && !day_present {
+			continue
+		}
 
-        log.Printf("tag:%s line:%s", tag, l)
-        if logger != nil {
-            logger.ch <- l
-        }
-        if day_logger != nil {
-            day_logger.ch <- l
-        }
-    }
+		l := line[index+1:]
+
+		log.Printf("tag:%s line:%s", tag, l)
+		if logger != nil {
+			logger.ch <- l
+		}
+		if day_logger != nil {
+			day_logger.ch <- l
+		}
+	}
 }
 
 func init_tags() {
-    tags["application"] = NewLogger("application")
-    tags["device"] = NewLogger("device")
-    tags["ng_application"] = NewLogger("ng_application")
-    day_tags["login"] = NewDailyLogger("login")
-    day_tags["payment"] = NewDailyLogger("payment")
+	tags["application"] = NewLogger("application")
+	tags["device"] = NewLogger("device")
+	tags["ng_application"] = NewLogger("ng_application")
+	tags["service"] = NewLogger("service")
+	day_tags["login"] = NewDailyLogger("login")
+	day_tags["payment"] = NewDailyLogger("payment")
 	day_tags["register"] = NewDailyLogger("register")
 	day_tags["level"] = NewDailyLogger("level")
 	day_tags["mission"] = NewDailyLogger("mission")
@@ -63,63 +64,63 @@ func init_tags() {
 }
 
 func read_cfg() {
-    app_cfg := make(map[string]string)
+	app_cfg := make(map[string]string)
 	err := cfg.Load("log.cfg", app_cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-    root, present := app_cfg["root"]
-    if !present {
-        fmt.Println("need config root directory")
-        os.Exit(1)
-    }
-    ROOT = root
+	root, present := app_cfg["root"]
+	if !present {
+		fmt.Println("need config root directory")
+		os.Exit(1)
+	}
+	ROOT = root
 
-    port, present := app_cfg["port"]
-    if !present {
-        fmt.Println("need config listen port")
-        os.Exit(1)
-    }
-    nport, err := strconv.Atoi(port)
-    if err != nil {
-        fmt.Println("need config listen port")
-        os.Exit(1)
-    }
-    PORT = nport
+	port, present := app_cfg["port"]
+	if !present {
+		fmt.Println("need config listen port")
+		os.Exit(1)
+	}
+	nport, err := strconv.Atoi(port)
+	if err != nil {
+		fmt.Println("need config listen port")
+		os.Exit(1)
+	}
+	PORT = nport
 	fmt.Printf("root:%s port:%d\n", ROOT, PORT)
 
-    if _, present = app_cfg["bind_addr"]; present {
-        BIND_ADDR = app_cfg["bind_addr"]
-    }
+	if _, present = app_cfg["bind_addr"]; present {
+		BIND_ADDR = app_cfg["bind_addr"]
+	}
 	fmt.Printf("root:%s bind addr:%s port:%d\n", ROOT, BIND_ADDR, PORT)
 
 }
 
 func main() {
-    read_cfg()
+	read_cfg()
 
-    log.SetFlags(log.Lshortfile|log.LstdFlags)
-    
-    init_tags()
-    for _, t := range tags {
-        go t.Run()
-    }
-    for _, t := range day_tags {
-        go t.Run()
-    }
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
-    ip := net.ParseIP(BIND_ADDR)
-    addr := net.TCPAddr{ip, PORT, ""}
-    listen, err := net.ListenTCP("tcp", &addr);
-    if err != nil {
-        fmt.Println("初始化失败", err.Error())
-        return
-    }
-    for {
-        client, err := listen.AcceptTCP();
-        if err != nil {
-            return
-        }
-        go handle_client(client)
-    }
+	init_tags()
+	for _, t := range tags {
+		go t.Run()
+	}
+	for _, t := range day_tags {
+		go t.Run()
+	}
+
+	ip := net.ParseIP(BIND_ADDR)
+	addr := net.TCPAddr{ip, PORT, ""}
+	listen, err := net.ListenTCP("tcp", &addr)
+	if err != nil {
+		fmt.Println("初始化失败", err.Error())
+		return
+	}
+	for {
+		client, err := listen.AcceptTCP()
+		if err != nil {
+			return
+		}
+		go handle_client(client)
+	}
 }
