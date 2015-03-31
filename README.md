16c48fe0a70121dfa3e29f4f85056224a6f42053
lost <dragon829@gmail.com>
2015-03-31 16:05:13 +0800

    自动根据标签创建目录

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Introduction
 ---------
 A line log server, every line log is appended to a file.
-The single log file is limited in 1GB.
+The single log file is limited in 1GB. if tag's directory name not exist, it will auto create.
 
+run server `./log_server log.cfg -log_dir="./log" -stderrthreshold=ERROR`
--- log.go
+++ log.go
@@ -1,114 +1,109 @@
 package main
 
-import "bufio"
-import "strings"
-import "net"
-import "fmt"
-import "os"
-import "strconv"
-import "log"
-import "github.com/jimlawless/cfg"
+import (
+    "bufio"
+    "strings"
+    "net"
+    "fmt"
+    "os"
+    "strconv"
+    "github.com/jimlawless/cfg"
+    log "github.com/golang/glog"
+    "sync"
+    "flag"
+)
 
 var ROOT = "/tmp/"
 var PORT = 24000
 var BIND_ADDR = ""
 
-var tags = map[string]*Logger{}
 var day_tags = map[string]*DailyLogger{}
+var lock sync.Mutex
 
 func handle_client(conn net.Conn) {
-	reader := bufio.NewReader(conn)
+    reader := bufio.NewReader(conn)
 
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            break
+        }
 
-		index := strings.IndexByte(line, ':')
-		if index == -1 {
-			continue
-		}
-		tag := line[0:index]
-		logger, present := tags[tag]
+        index := strings.IndexByte(line, ':')
+        if index == -1 {
+            continue
+        }
+        tag := line[0:index]
 
-		day_logger, day_present := day_tags[tag]
-		if !present && !day_present {
-			continue
-		}
+        day_logger, day_present := day_tags[tag]
 
-		l := line[index+1:]
+        if !day_present {
+            lock.Lock()
+            day_tags[tag] = NewDailyLogger(tag)
+            go day_tags[tag].Run()
+            lock.Unlock()
+            day_logger = day_tags[tag]
+        }
 
-		log.Printf("tag:%s line:%s", tag, l)
-		if logger != nil {
-			logger.ch <- l
-		}
-		if day_logger != nil {
-			day_logger.ch <- l
-		}
-	}
+        l := line[index+1:]
+
+        log.Infof("tag:%s line:%s", tag, l)
+        if day_logger != nil {
+            day_logger.ch <- l
+        }
+    }
 }
 
-func init_tags() {
-	tags["application"] = NewLogger("application")
-	tags["device"] = NewLogger("device")
-	tags["ng_application"] = NewLogger("ng_application")
-	tags["service"] = NewLogger("service")
-	tags["alarm_info"] = NewLogger("alarm_info")
-	day_tags["login"] = NewDailyLogger("login")
-	day_tags["payment"] = NewDailyLogger("payment")
-	day_tags["register"] = NewDailyLogger("register")
-	day_tags["level"] = NewDailyLogger("level")
-	day_tags["mission"] = NewDailyLogger("mission")
-	day_tags["consumption"] = NewDailyLogger("consumption")
-	day_tags["coin"] = NewDailyLogger("coin")
-}
+func read_cfg(filename string) {
+    app_cfg := make(map[string]string)
+    err := cfg.Load(filename, app_cfg)
+    if err != nil {
+        log.Fatal(err)
+    }
+    root, present := app_cfg["root"]
+    if !present {
+        fmt.Println("need config root directory")
+        os.Exit(1)
+    }
+    ROOT = root
 
-func read_cfg() {
-	app_cfg := make(map[string]string)
-	err := cfg.Load("log.cfg", app_cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	root, present := app_cfg["root"]
-	if !present {
-		fmt.Println("need config root directory")
-		os.Exit(1)
-	}
-	ROOT = root
+    port, present := app_cfg["port"]
+    if !present {
+        fmt.Println("need config listen port")
+        os.Exit(1)
+    }
+    nport, err := strconv.Atoi(port)
+    if err != nil {
+        fmt.Println("need config listen port")
+        os.Exit(1)
+    }
+    PORT = nport
+    fmt.Printf("root:%s port:%d\n", ROOT, PORT)
 
-	port, present := app_cfg["port"]
-	if !present {
-		fmt.Println("need config listen port")
-		os.Exit(1)
-	}
-	nport, err := strconv.Atoi(port)
-	if err != nil {
-		fmt.Println("need config listen port")
-		os.Exit(1)
-	}
-	PORT = nport
-	fmt.Printf("root:%s port:%d\n", ROOT, PORT)
-
-	if _, present = app_cfg["bind_addr"]; present {
-		BIND_ADDR = app_cfg["bind_addr"]
-	}
-	fmt.Printf("root:%s bind addr:%s port:%d\n", ROOT, BIND_ADDR, PORT)
+    if _, present = app_cfg["bind_addr"]; present {
+        BIND_ADDR = app_cfg["bind_addr"]
+    }
+    fmt.Printf("root:%s bind addr:%s port:%d\n", ROOT, BIND_ADDR, PORT)
 
 }
 
 func main() {
-	read_cfg()
+    flag.Parse()
 
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
+    var filename string
+    if len(flag.Args()) == 0 {
+        filename = "log.cfg"
+        if _, err := os.Stat(filename); err != nil {
+            filename = ""
+        }
+    } else {
+        filename = flag.Args()[0]
+    }
 
-	init_tags()
-	for _, t := range tags {
-		go t.Run()
-	}
-	for _, t := range day_tags {
-		go t.Run()
-	}
-	addr := fmt.Sprintf("%s:%d", BIND_ADDR, PORT)
-	Serve(addr, handle_client)
+    if filename != "" {
+        read_cfg(filename)
+    }
+
+    addr := fmt.Sprintf("%s:%d", BIND_ADDR, PORT)
+    Serve(addr, handle_client)
 }
